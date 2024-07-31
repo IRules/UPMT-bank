a16a1486732c341ce741704f8fdb9d6a04564ac9
IRules <48042380+IRules@users.noreply.github.com>
2024-08-01 01:56:54 +0300

    Fixed errors x3.

--- src/pages/api/receive-transfer.ts
+++ src/pages/api/receive-transfer.ts
@@ -1,9 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type {NextApiRequest, NextApiResponse} from "next";
 import db, {dbSession} from "@/lib/surrealdb";
 
-
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
@@ -11,48 +10,55 @@ export default function handler(
         return res.status(405).end();
     }
 
-    const {transactionId, debtorAccount, creditorAccount, instructedAmount} = req.body;
+    if(!db.status) {
+        await dbSession.then(
+            (session) => {
+                console.log('Connected to SurrealDB');
+            }
+        ).catch(
+            (error) => {
+                console.error('Failed to connect to SurrealDB', error);
+            }
+        );
+    }
 
-    return dbSession.then(
-        async () => {
-            return db.query(`SELECT * FROM transactions WHERE transactionId = "${transactionId}"`).then(
-                (result) => {
-                    //@ts-ignore
-                    if (result[0].length > 0) {
-                        return res.status(400).json({ outcome: "REJECTED" });
-                    } else {
-                        db.create("transactions", {
-                            transactionId,
-                            debtorAccount,
-                            creditorAccount,
-                            instructedAmount,
-                        }).then(
-                            async () => {
-                                return db.query(`SELECT * FROM accounts WHERE iban = "${creditorAccount.iban}"`).then(
-                                    (result) => {
-                                        //@ts-ignore
-                                        if (result[0].length === 0) {
-                                            return res.status(400).json({ outcome: "REJECTED" });
-                                        } else {
-                                            console.log(instructedAmount.amount)
-                                            //@ts-ignore
-                                            const balance = result[0][0].balance + instructedAmount.amount;
-                                            db.update("accounts", {
-                                                iban: creditorAccount.iban,
-                                                balance,
-                                            }).then(
-                                                () => {
-                                                    return res.status(200).json({ outcome: "ACCEPTED" });
-                                                }
-                                            )
+    const {transactionId, debtorAccount, creditorAccount, instructedAmount} = req.body;
+    return db.query(`SELECT * FROM transactions WHERE transactionId = "${transactionId}"`).then(
+        (result) => {
+            //@ts-ignore
+            if (result[0].length > 0) {
+                return res.status(400).json({outcome: "REJECTED"});
+            } else {
+                db.create("transactions", {
+                    transactionId,
+                    debtorAccount,
+                    creditorAccount,
+                    instructedAmount,
+                }).then(
+                    async () => {
+                        return db.query(`SELECT * FROM accounts WHERE iban = "${creditorAccount.iban}"`).then(
+                            (result) => {
+                                //@ts-ignore
+                                if (result[0].length === 0) {
+                                    return res.status(400).json({outcome: "REJECTED"});
+                                } else {
+                                    console.log(instructedAmount.amount)
+                                    //@ts-ignore
+                                    const balance = result[0][0].balance + instructedAmount.amount;
+                                    db.update("accounts", {
+                                        iban: creditorAccount.iban,
+                                        balance,
+                                    }).then(
+                                        () => {
+                                            return res.status(200).json({outcome: "ACCEPTED"});
                                         }
-                                    }
-                                )
+                                    )
+                                }
                             }
                         )
                     }
-                }
-            )
+                )
+            }
         }
     )
 }
