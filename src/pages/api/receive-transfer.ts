35325d407fbfeb7491fe8e22510c5f292c708c51
IRules <48042380+IRules@users.noreply.github.com>
2024-08-01 04:40:20 +0300

    Fixed even more errors x4.

--- src/pages/api/receive-transfer.ts
+++ src/pages/api/receive-transfer.ts
@@ -36,6 +36,7 @@ export default async function handler(
                     debtorAccount,
                     creditorAccount,
                     instructedAmount,
+                    timestamp: new Date()
                 }).then(
                     async () => {
                         return db.query(`SELECT * FROM accounts WHERE iban = "${creditorAccount.iban}"`).then(
